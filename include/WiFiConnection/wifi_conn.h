2239d3b35e06fc6ca90283a641781513915c0584
Benas Urm <benas.urmanavicius@gmail.com>
2024-08-23 16:39:07 +0300

    Sensitive info deleted

--- include/WiFiConnection/wifi_conn.h
+++ include/WiFiConnection/wifi_conn.h
@@ -16,8 +16,8 @@ const char try_again_msg[] = "** Try connecting to network again";
 // Declaring SSID and password char array structure
 struct
 {
-    char ssid[msg_size] = "Sensor-Fi";
-    char passwd[msg_size] = "Bmp/@J*r;5_35U#W";
+    char ssid[msg_size] = "";
+    char passwd[msg_size] = "";
 } curr_net_info;
 
 void ReadNetInfo();
